d27213e4ae63a38ae91a44681e2ff6741bcee268
Code for TODA <info@codefortoda.org>
2020-05-19 05:49:52 +0900

    日付書式判定変更

--- main.py
+++ main.py
@@ -91,11 +91,11 @@ def export_data_json():
 
     jokyo_path = get_csv(settings.JOKYO_URL, settings.JOKYO_TITLE)
     df_kanja = pd.read_csv(jokyo_path, encoding="cp932")
-    # df_kanja["date"] = pd.to_datetime(dt_now.strftime("%Y") + "年" + df_kanja["判明日"], format="%Y年%m月%d日", errors="coerce")
-    if len(df_kanja.iloc[0, 1].split(',')[0]) == 2:
-        df_kanja["date"] = pd.to_datetime(df_kanja["判明日"], format="%y/%m/%d", errors="coerce")
-    else:
-        df_kanja["date"] = pd.to_datetime(df_kanja["判明日"], format="%Y/%m/%d", errors="coerce")
+    df_kanja["date"] = df_kanja["判明日"].apply(
+        lambda x: pd.to_datetime(x, errors="coerce")
+        if len(x.split(',')[0]) != 2
+        else pd.to_datetime(x, format="%y/%m/%d", errors="coerce")
+    )
     df_patients_sum = (
         df_kanja["date"].value_counts().sort_index().asfreq("D", fill_value=0).reset_index()
     )
