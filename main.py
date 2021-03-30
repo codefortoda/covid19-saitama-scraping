2dd213c3c47d5f780c6aba6547edad1d5cda22c4
Code for TODA <info@codefortoda.org>
2021-03-31 03:19:07 +0900

    検査陽性者内訳 単位漏れ対応

--- main.py
+++ main.py
@@ -87,13 +87,13 @@ def export_data_json():
     for i in re.finditer(r"(自治体による検査|民間検査機関等による検査)[\(（]\d{1,2}月\d{1,2}日まで[\)）]：延べ([0-9,]+)人", text):
         temp[i.group(1)] = int(i.group(2).replace(",", ""))
 
-    m = re.search("(入院)：(指定医療機関)([0-9,]+)人\s*(一般医療機関)([0-9,]+)人\s*(計)([0-9,]+)人", text)
+    m = re.search("(入院)：(指定医療機関)([0-9,]+)人?\s*(一般医療機関)([0-9,]+)人\s*(計)([0-9,]+)人?", text)
     if m:
         temp[f"{m.group(1)}_{m.group(2)}"] = int(m.group(3).replace(",", ""))
         temp[f"{m.group(1)}_{m.group(4)}"] = int(m.group(5).replace(",", ""))
         temp[f"{m.group(1)}_{m.group(6)}"] = int(m.group(7).replace(",", ""))
 
-    m = re.search("(退院・療養終了)：(退院)([0-9,]+)人\s*(療養終了)([0-9,]+)人\s*(計)([0-9,]+)人?", text)
+    m = re.search("(退院・療養終了)：(退院)([0-9,]+)人?\s*(療養終了)([0-9,]+)人\s*(計)([0-9,]+)人?", text)
     if m:
         temp[f"{m.group(1)}_{m.group(2)}"] = int(m.group(3).replace(",", ""))
         temp[f"{m.group(1)}_{m.group(4)}"] = int(m.group(5).replace(",", ""))
